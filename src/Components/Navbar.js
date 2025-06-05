dfab65a2ff77d085fae9e91ae58177d7836d3d0e
Aryan <aryanmishr291@gmail.com>
2025-06-05 23:57:33 +0530

    new commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur-md shadow-sm z-50 p-4 flex justify-center space-x-2 border-b border-slate-200">
-      <a href="#home" className="text-sky-600 font-medium hover:underline">Home</a>
-      <a href="#skills" className="text-sky-600 font-medium hover:underline">Skills</a>
-      <a href="#experience" className="text-sky-600 font-medium hover:underline">Experience</a>
-      <a href="#project" className="text-sky-600 font-medium hover:underline">Project</a>
-      <a href="#achievements" className="text-sky-600 font-medium hover:underline">Achievements</a>
+      <a href="#home" className="text-sky-600 font-medium hover:underline border-2 rounded-lg p-2 border-sky-200">Home</a>
+      <a href="#skills" className="text-sky-600 font-medium hover:underline border-2 rounded-lg p-2 border-sky-200">Skills</a>
+      <a href="#experience" className="text-sky-600 font-medium hover:underline border-2 rounded-lg p-2 border-sky-200">Experience</a>
+      <a href="#project" className="text-sky-600 font-medium hover:underline border-2 rounded-lg p-2 border-sky-200">Project</a>
+      <a href="#achievements" className="text-sky-600 font-medium hover:underline border-2 rounded-lg p-2 border-sky-200">Achievements</a>
       
     </nav>
   )
